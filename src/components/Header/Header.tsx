e680515668b1cbfa67bf1f808c967e5b43698ea3
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-01-28 22:59:54 +0300

    Header component update (#3)
    * feat(header): add and style search input with lucide icons
    
    * feat(header): add and style header buttons
    
    * feat(header): add burger menu toggle button and addaptive design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,47 @@
+import { Heart, Search, ShoppingCart, User, X } from 'lucide-react';
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <div>
-          <h1 className={styles.logo}>TechStore</h1>
+        <div className={styles.logoWrap}>
+          <a href="#" className={styles.logo}>
+            TechStore
+          </a>
         </div>
-        <div className={styles.searchBar}>
-          <input type="text" placeholder="Искать в TechStore" />
+
+        <button className={styles.menuToggle}>
+          <span className={styles.burger}></span>
+          <span className={styles.burger}></span>
+          <span className={styles.burger}></span>
+        </button>
+
+        <div className={styles.searchHolder}>
+          <span className={styles.searchIcon}>
+            <Search color="#909cb2" size={23} />
+          </span>
+          <input
+            className={styles.search}
+            type="text"
+            placeholder="Искать в TechStore"
+          />
+          <button className={styles.closeButton}>
+            <X color="#909cb2" size={23} />
+          </button>
         </div>
-        <div className={styles.buttons}>buttons</div>
+
+        <div className={styles.buttons}>
+          <button className={styles.button}>
+            <Heart color="#4a5568" size={27} />
+          </button>
+          <button className={styles.button}>
+            <ShoppingCart color="#4a5568" size={27} />
+          </button>
+          <button className={styles.button}>
+            <User color="#4a5568" size={27} />
+          </button>
+        </div>
       </div>
     </header>
   );
