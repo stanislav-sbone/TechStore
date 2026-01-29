1eff1e7df1aa81982275f9d4170a5b67a7b2c8c3
Станислав Борисенко <stas.borisenko91@gmail.com>
2026-01-29 22:15:02 +0300

    feat: add fetchProducts function with 1s delay

---
+++ src/services/api.ts
@@ -0,0 +1,10 @@
+import { products } from '../data/products';
+import type { Product } from '../types/product';
+
+export const fetchProducts = async (): Promise<Product[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(products);
+    }, 1000);
+  });
+};
