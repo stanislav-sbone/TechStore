25c63cd01a3b381e03bf60abe9477b4018385250
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-02-09 16:22:21 +0300

    feat(header-buttons): wrap header icons with router links

--- src/components/Header/HeaderButtons.tsx
+++ src/components/Header/HeaderButtons.tsx
@@ -1,18 +1,26 @@
 import { Heart, ShoppingCart, User } from 'lucide-react';
 import styles from './Header.module.css';
+import { Link } from 'react-router';
+import { ROUTES } from '@/routes/constants/routes';
 
 const HeaderButtons = () => {
   return (
     <div className={styles.buttons}>
-      <button className={styles.button}>
-        <Heart color="#4a5568" size={27} />
-      </button>
-      <button className={styles.button}>
-        <ShoppingCart color="#4a5568" size={27} />
-      </button>
-      <button className={styles.button}>
-        <User color="#4a5568" size={27} />
-      </button>
+      <Link to={ROUTES.FAVORITES}>
+        <button className={styles.button}>
+          <Heart color="#4a5568" size={27} />
+        </button>
+      </Link>
+      <Link to={ROUTES.CART}>
+        <button className={styles.button}>
+          <ShoppingCart color="#4a5568" size={27} />
+        </button>
+      </Link>
+      <Link to={ROUTES.PROFILE}>
+        <button className={styles.button}>
+          <User color="#4a5568" size={27} />
+        </button>
+      </Link>
     </div>
   );
 };
