70bddeb9328191eed99ee9f1401d49e06f702311
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-02-09 22:49:46 +0300

    feat(not-found): update UI and UX of component
    - replace Link with HomeLink component
    - add submessage
    - change styles of messages

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,14 +1,15 @@
-import { Link } from 'react-router';
 import styles from './NotFound.module.css';
+import { HomeLink } from '@/components/HomeLink';
 
 const NotFound = () => {
   return (
     <section className={styles.notFound}>
       <img src="/notfound.png" alt="notFound" className={styles.image} />
-      <p className={styles.text}>Страница не найдена</p>
-      <Link to="/" className={styles.goBack}>
-        Вернуться на главную
-      </Link>
+      <p className={styles.message}>Страница не найдена</p>
+      <p className={styles.submessage}>
+        Вероятно страницы не существует, или она находится в разработке
+      </p>
+      <HomeLink />
     </section>
   );
 };
