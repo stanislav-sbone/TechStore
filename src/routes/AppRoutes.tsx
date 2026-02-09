7f9dddfee0dca8510268c84308aa10add1046329
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-02-09 16:44:46 +0300

    feat(favorites-page): add page of favorite products

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,13 @@ import { Routes, Route } from 'react-router';
 import { ROUTES } from '@/routes/constants/routes';
 import { Home } from '@/pages/Home';
 import { NotFound } from '@/pages/NotFound';
+import { Favorites } from '@/pages/Favorites';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />}></Route>
-      {/* <Route path={ROUTES.FAVORITES} element={<Favorites />}></Route> */}
+      <Route path={ROUTES.FAVORITES} element={<Favorites />}></Route>
       {/* <Route path={ROUTES.CART} element={<Cart />}></Route> */}
       {/* <Route path={ROUTES.PRODUCT} element={<Product />}></Route> */}
       {/* <Route path={ROUTES.PROFILE} element={<Profile />}></Route> */}
