d8826677964412c5d29d42290cfcd89aacb18a93
Станислав Борисенко <stas.borisenko91@gmail.com>
2026-02-09 23:40:30 +0300

    feat(header-buttons): add badge to favorites link

--- src/components/Header/HeaderButtons.tsx
+++ src/components/Header/HeaderButtons.tsx
@@ -2,13 +2,21 @@ import { Heart, ShoppingCart, User } from 'lucide-react';
 import styles from './Header.module.css';
 import { Link } from 'react-router';
 import { ROUTES } from '@/routes/constants/routes';
+import { useAppSelector } from '@/store/hooks';
 
 const HeaderButtons = () => {
+  const favorites = useAppSelector((state) => state.favorites.items);
+
   return (
     <div className={styles.buttons}>
       <Link to={ROUTES.FAVORITES}>
         <button className={styles.button}>
           <Heart color="#4a5568" size={27} />
+          {favorites.length > 0 && (
+            <span className={styles.badge}>
+              {favorites.length > 99 ? '99' : favorites.length}
+            </span>
+          )}
         </button>
       </Link>
       <Link to={ROUTES.CART}>
