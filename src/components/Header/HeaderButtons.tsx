69c64e053e6da4d28c9772947bdde81ab59c6437
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-02-15 00:44:42 +0300

    feat(header): add badge to cart button (#60)

--- src/components/Header/HeaderButtons.tsx
+++ src/components/Header/HeaderButtons.tsx
@@ -6,28 +6,30 @@ import { useAppSelector } from '@/store/hooks';
 
 const HeaderButtons = () => {
   const favorites = useAppSelector((state) => state.favorites.items);
+  const cart = useAppSelector((state) => state.cart.items);
+
+  const cartQuantity = cart.reduce((acc, cur) => acc + cur.quantity, 0);
 
   return (
     <div className={styles.buttons}>
-      <Link to={ROUTES.FAVORITES}>
-        <button className={styles.button}>
-          <Heart color="#4a5568" size={27} />
-          {favorites.length > 0 && (
-            <span className={styles.badge}>
-              {favorites.length > 99 ? '99' : favorites.length}
-            </span>
-          )}
-        </button>
+      <Link to={ROUTES.FAVORITES} className={styles.buttonLink}>
+        <Heart color="#4a5568" size={27} />
+        {favorites.length > 0 && (
+          <span className={styles.badge}>
+            {favorites.length > 99 ? '99' : favorites.length}
+          </span>
+        )}
       </Link>
-      <Link to={ROUTES.CART}>
-        <button className={styles.button}>
-          <ShoppingCart color="#4a5568" size={27} />
-        </button>
+      <Link to={ROUTES.CART} className={styles.buttonLink}>
+        <ShoppingCart color="#4a5568" size={27} />
+        {cartQuantity > 0 && (
+          <span className={styles.badge}>
+            {cartQuantity > 99 ? '99' : cartQuantity}
+          </span>
+        )}
       </Link>
-      <Link to={ROUTES.PROFILE}>
-        <button className={styles.button}>
-          <User color="#4a5568" size={27} />
-        </button>
+      <Link to={ROUTES.PROFILE} className={styles.buttonLink}>
+        <User color="#4a5568" size={27} />
       </Link>
     </div>
   );
