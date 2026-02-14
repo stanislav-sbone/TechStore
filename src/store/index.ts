666783a8cf472a4e09c823d62b9ba9727ae3369f
Станислав Борисенко <stas.borisenko91@gmail.com>
2026-02-14 21:44:51 +0300

    feat: create redux cart slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from './features/products/productsSlice';
 import favoritesReducer from './features/favorites/favoritesSlice';
+import cartReducer from './features/cart/cartSlice';
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
     favorites: favoritesReducer,
+    cart: cartReducer,
   },
 });
 
