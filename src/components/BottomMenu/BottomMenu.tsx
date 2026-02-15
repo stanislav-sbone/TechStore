ae1c711d22ec7cd5bbd3463ecb77698242953136
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-02-15 19:45:16 +0300

    feat(bottom-menu): add styles for active link (#68)

--- src/components/BottomMenu/BottomMenu.tsx
+++ src/components/BottomMenu/BottomMenu.tsx
@@ -1,24 +1,61 @@
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
 import styles from './BottomMenu.module.css';
 import { Home, Heart, ShoppingCart, User } from 'lucide-react';
 
 const BottomMenu = () => {
+  const { pathname } = useLocation();
+
   return (
     <nav className={styles.bottomMenu}>
-      <Link to="/" className={styles.link}>
-        <Home color="#4a5568" size={24} />
+      <Link
+        to="/"
+        className={
+          pathname === '/' ? `${styles.activeLink} ${styles.link}` : styles.link
+        }
+      >
+        <Home color={pathname === '/' ? '#3182ce' : '#4a5568'} size={24} />
         <span className={styles.label}>Главная</span>
       </Link>
-      <Link to="/favorites" className={styles.link}>
-        <Heart color="#4a5568" size={24} />
+      <Link
+        to="/favorites"
+        className={
+          pathname === '/favorites'
+            ? `${styles.activeLink} ${styles.link}`
+            : styles.link
+        }
+      >
+        <Heart
+          color={pathname === '/favorites' ? '#3182ce' : '#4a5568'}
+          size={24}
+        />
         <span className={styles.label}>Избранное</span>
       </Link>
-      <Link to="/cart" className={styles.link}>
-        <ShoppingCart color="#4a5568" size={24} />
+      <Link
+        to="/cart"
+        className={
+          pathname === '/cart'
+            ? `${styles.activeLink} ${styles.link}`
+            : styles.link
+        }
+      >
+        <ShoppingCart
+          color={pathname === '/cart' ? '#3182ce' : '#4a5568'}
+          size={24}
+        />
         <span className={styles.label}>Корзина</span>
       </Link>
-      <Link to="/profile" className={styles.link}>
-        <User color="#4a5568" size={24} />
+      <Link
+        to="/profile"
+        className={
+          pathname === '/profile'
+            ? `${styles.activeLink} ${styles.link}`
+            : styles.link
+        }
+      >
+        <User
+          color={pathname === '/profile' ? '#3182ce' : '#4a5568'}
+          size={24}
+        />
         <span className={styles.label}>Профиль</span>
       </Link>
     </nav>
