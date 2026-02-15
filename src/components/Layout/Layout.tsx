40aa47bf82898d2d6c26a59e38dc99c80ffc40ac
Станислав Борисенко <stas.borisenko91@gmail.com>
2026-02-15 19:30:54 +0300

    feat: add bottom-menu - add and style bottom-menu component if screen width < 600px - remove burger menu from header - add new selector to main

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,19 +2,23 @@ import type { FC, ReactNode } from 'react';
 import styles from './Layout.module.css';
 import { Header } from '@/components/Header';
 import { ScrollToTop } from '../ScrollToTop';
+import { BottomMenu } from '../BottomMenu';
+import useIsMobile from '@/hooks/useIsMobile';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout: FC<LayoutProps> = ({ children }) => {
+  const isMobile = useIsMobile(600);
   return (
     <>
       <Header />
       <ScrollToTop />
-      <main className={styles.main}>
+      <main className={`${styles.main} ${isMobile ? styles.mainMobile : ''}`}>
         <div className={styles.container}>{children}</div>
       </main>
+      {isMobile && <BottomMenu />}
     </>
   );
 };
