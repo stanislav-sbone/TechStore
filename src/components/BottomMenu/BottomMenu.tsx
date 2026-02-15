5bffa6f39e28b458447a9b2ccd9d0b4f9377d343
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-02-15 20:31:28 +0300

    feat(bottom-menu): add badges to nav items (#70)
    - add badge to favorite link button
    - add badge to cart link button

--- src/components/BottomMenu/BottomMenu.tsx
+++ src/components/BottomMenu/BottomMenu.tsx
@@ -1,14 +1,28 @@
 import { Link, useLocation } from 'react-router';
 import { navItems } from '@/constants/bottomNav';
 import styles from './BottomMenu.module.css';
+import { useAppSelector } from '@/store/hooks';
+import { ROUTES } from '@/routes/constants/routes';
 
 const BottomMenu = () => {
   const { pathname } = useLocation();
+  const favorites = useAppSelector((state) => state.favorites.items);
+  const cart = useAppSelector((state) => state.cart.items);
+
+  const cartQuantity = cart.reduce((acc, cur) => acc + cur.quantity, 0);
+
+  const getBadgeQuantity = (path: string) => {
+    if (path === ROUTES.FAVORITES) return favorites.length;
+    if (path === ROUTES.CART) return cartQuantity;
+
+    return 0;
+  };
 
   return (
     <nav className={styles.bottomMenu}>
       {navItems.map((item) => {
         const isActive = item.path === pathname;
+        const badgeCount = getBadgeQuantity(item.path);
 
         return (
           <Link
@@ -18,7 +32,12 @@ const BottomMenu = () => {
               isActive ? `${styles.activeLink} ${styles.link}` : styles.link
             }
           >
-            <item.icon color={isActive ? '#3182ce' : '#4a5568'} size={24} />
+            <div className={styles.iconWrapper}>
+              <item.icon color={isActive ? '#3182ce' : '#4a5568'} size={24} />
+              {badgeCount !== 0 && (
+                <span className={styles.badge}>{badgeCount}</span>
+              )}
+            </div>
             <span className={styles.label}>{item.label}</span>
           </Link>
         );
