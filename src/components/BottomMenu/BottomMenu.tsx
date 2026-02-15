0881e6f9e444bd7d6b0d72583dbd2727baba1ebb
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-02-15 20:00:37 +0300

    refactor(bottom-menu): generate nav items dynamically with map (#69)

--- src/components/BottomMenu/BottomMenu.tsx
+++ src/components/BottomMenu/BottomMenu.tsx
@@ -1,63 +1,28 @@
 import { Link, useLocation } from 'react-router';
+import { navItems } from '@/constants/bottomNav';
 import styles from './BottomMenu.module.css';
-import { Home, Heart, ShoppingCart, User } from 'lucide-react';
 
 const BottomMenu = () => {
   const { pathname } = useLocation();
 
   return (
     <nav className={styles.bottomMenu}>
-      <Link
-        to="/"
-        className={
-          pathname === '/' ? `${styles.activeLink} ${styles.link}` : styles.link
-        }
-      >
-        <Home color={pathname === '/' ? '#3182ce' : '#4a5568'} size={24} />
-        <span className={styles.label}>Главная</span>
-      </Link>
-      <Link
-        to="/favorites"
-        className={
-          pathname === '/favorites'
-            ? `${styles.activeLink} ${styles.link}`
-            : styles.link
-        }
-      >
-        <Heart
-          color={pathname === '/favorites' ? '#3182ce' : '#4a5568'}
-          size={24}
-        />
-        <span className={styles.label}>Избранное</span>
-      </Link>
-      <Link
-        to="/cart"
-        className={
-          pathname === '/cart'
-            ? `${styles.activeLink} ${styles.link}`
-            : styles.link
-        }
-      >
-        <ShoppingCart
-          color={pathname === '/cart' ? '#3182ce' : '#4a5568'}
-          size={24}
-        />
-        <span className={styles.label}>Корзина</span>
-      </Link>
-      <Link
-        to="/profile"
-        className={
-          pathname === '/profile'
-            ? `${styles.activeLink} ${styles.link}`
-            : styles.link
-        }
-      >
-        <User
-          color={pathname === '/profile' ? '#3182ce' : '#4a5568'}
-          size={24}
-        />
-        <span className={styles.label}>Профиль</span>
-      </Link>
+      {navItems.map((item) => {
+        const isActive = item.path === pathname;
+
+        return (
+          <Link
+            to={item.path}
+            key={item.path}
+            className={
+              isActive ? `${styles.activeLink} ${styles.link}` : styles.link
+            }
+          >
+            <item.icon color={isActive ? '#3182ce' : '#4a5568'} size={24} />
+            <span className={styles.label}>{item.label}</span>
+          </Link>
+        );
+      })}
     </nav>
   );
 };
