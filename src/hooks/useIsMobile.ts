a1727d83347ab163bd49276855283c29bbe8a639
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-02-15 20:36:18 +0300

    fix(useIsMobile): correct useEffect cleanup function (#71)

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -6,7 +6,7 @@ const useIsMobile = (breakpoint: number) => {
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth <= breakpoint);
     window.addEventListener('resize', handleResize);
-    return window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, [breakpoint]);
 
   return isMobile;
