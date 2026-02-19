70c5a4e7ea507e3fd44dd6eb0547717e0638cb1f
Станислав Борисенко <stas.borisenko91@gmail.com>
2026-02-19 21:26:53 +0300

    feat(empty-cart): add and style empty cart component

---
+++ src/pages/Cart/components/EmptyCart/EmptyCart.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router';
+import styles from './EmptyCart.module.css';
+
+const EmptyCart = () => {
+  return (
+    <div className={styles.empty}>
+      <img src="/EmptyCart.png" alt="EmptyCart" className={styles.image} />
+      <h2 className={styles.text}>Ваша корзина пуста</h2>
+      <p className={styles.subtext}>
+        Добавьте товары из каталога, чтобы оформить заказ.
+      </p>
+      <button className={styles.button} type="button">
+        <Link to="/">Перейти к покупкам</Link>
+      </button>
+    </div>
+  );
+};
+
+export default EmptyCart;
