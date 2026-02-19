2c2d9f9c9772da929ba72b1a99357822dfa5113d
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-02-19 20:18:25 +0300

    feat(cart-summary): add rows for summary and make position sticky (#81)

--- src/pages/Cart/components/CartSummary/CartSummary.tsx
+++ src/pages/Cart/components/CartSummary/CartSummary.tsx
@@ -3,21 +3,40 @@ import styles from './CartSummary.module.css';
 
 interface CartSummaryProps {
   cartQuantity: number;
+  sumPriceWithDiscount: number;
   sumPrice: number;
 }
 
-const CartSummary: FC<CartSummaryProps> = ({ cartQuantity, sumPrice }) => {
+const CartSummary: FC<CartSummaryProps> = ({
+  cartQuantity,
+  sumPriceWithDiscount,
+  sumPrice,
+}) => {
   return (
     <aside className={styles.summary}>
       <h2 className={styles.summaryTitle}>Итого</h2>
       <div className={styles.summaryRow}>
-        <span className={styles.summaryLabel}>Товары</span>
+        <span className={styles.summaryLabel}>Количество товаров</span>
         <span className={styles.summaryValue}>{cartQuantity}</span>
+      </div>
+      <div className={styles.summaryRow}>
+        <span className={styles.summaryLabel}>Сумма заказа без скидки</span>
+        <span className={styles.summaryValue}>
+          {sumPrice.toLocaleString('ru-RU')} ₽
+        </span>
+      </div>
+      <div className={styles.summaryRow}>
+        <span className={styles.summaryLabel}>Скидка</span>
+        <span className={styles.summaryValue}>
+          <span className={styles.discount}>
+            -{(sumPrice - sumPriceWithDiscount).toLocaleString('ru-RU')} ₽
+          </span>
+        </span>
       </div>
       <div className={styles.summaryRow}>
         <span className={styles.summaryLabel}>Сумма заказа</span>
         <span className={styles.summaryValue}>
-          {sumPrice.toLocaleString('ru-RU')} ₽
+          {sumPriceWithDiscount.toLocaleString('ru-RU')} ₽
         </span>
       </div>
 
