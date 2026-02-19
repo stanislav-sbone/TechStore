bd18ab6b90dc31b6fa55d5704ae515b385bb903b
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-02-19 16:29:55 +0300

    feat(cart-page): add and style cart page (#75)

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import { Home } from '@/pages/Home';
 import { NotFound } from '@/pages/NotFound';
 import { Favorites } from '@/pages/Favorites';
 import { ProductPage } from '@/pages/ProductPage';
+import { Cart } from '@/pages/Cart';
 
 const AppRoutes = () => {
   return (
@@ -11,7 +12,7 @@ const AppRoutes = () => {
       <Route path={ROUTES.HOME} element={<Home />}></Route>
       <Route path={ROUTES.FAVORITES} element={<Favorites />}></Route>
       <Route path={ROUTES.PRODUCT} element={<ProductPage />}></Route>
-      {/* <Route path={ROUTES.CART} element={<Cart />}></Route> */}
+      <Route path={ROUTES.CART} element={<Cart />}></Route>
       {/* <Route path={ROUTES.PROFILE} element={<Profile />}></Route> */}
       <Route path="*" element={<NotFound />}></Route>
     </Routes>
