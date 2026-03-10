1baeb8e9d852b9c02a54b0edb96db99c4e4ec3ff
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-03-10 15:50:10 +0300

    refactor(routes): simplify page imports with barrel file (#112)

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,24 +1,26 @@
 import { Routes, Route } from 'react-router';
 import { ROUTES } from '@/routes/constants/routes';
-import { Home } from '@/pages/Home';
-import { NotFound } from '@/pages/NotFound';
-import { Favorites } from '@/pages/Favorites';
-import { ProductPage } from '@/pages/ProductPage';
-import { Cart } from '@/pages/Cart';
-import { Login } from '@/pages/Login';
-import { Register } from '@/pages/Register';
+import {
+  Cart,
+  Favorites,
+  Home,
+  Login,
+  NotFound,
+  ProductPage,
+  Register,
+} from '@/pages';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path={ROUTES.HOME} element={<Home />}></Route>
-      <Route path={ROUTES.FAVORITES} element={<Favorites />}></Route>
-      <Route path={ROUTES.PRODUCT} element={<ProductPage />}></Route>
-      <Route path={ROUTES.CART} element={<Cart />}></Route>
-      <Route path={ROUTES.LOGIN} element={<Login />}></Route>
-      <Route path={ROUTES.REGISTER} element={<Register />}></Route>
-      {/* <Route path={ROUTES.PROFILE} element={<Profile />}></Route> */}
-      <Route path="*" element={<NotFound />}></Route>
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.FAVORITES} element={<Favorites />} />
+      <Route path={ROUTES.PRODUCT} element={<ProductPage />} />
+      <Route path={ROUTES.CART} element={<Cart />} />
+      <Route path={ROUTES.LOGIN} element={<Login />} />
+      <Route path={ROUTES.REGISTER} element={<Register />} />
+      {/* <Route path={ROUTES.PROFILE} element={<Profile />} /> */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
