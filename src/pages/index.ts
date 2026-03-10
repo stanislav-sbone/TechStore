0bce930caf406e45922857a47bf5f1b89d83103a
Станислав Борисенко <stas.borisenko91@gmail.com>
2026-03-10 15:49:17 +0300

    refactor(routes): simplify page imports with barrel file

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export { Home } from './Home';
+export { Cart } from './Cart';
+export { Favorites } from './Favorites';
+export { Login } from './Login';
+export { Register } from './Register';
+export { ProductPage } from './ProductPage';
+export { NotFound } from './NotFound';
