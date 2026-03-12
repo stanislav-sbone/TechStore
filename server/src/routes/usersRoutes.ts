25862cf93bd51ba24269b43792a0c95cfe4451ef
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-03-12 23:37:17 +0300

    feat(server): add endpoint for getting user data (#139)

--- server/src/routes/usersRoutes.ts
+++ server/src/routes/usersRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { authenticate } from '../middlewares/authenticate';
-import { authCompleteProfile } from '../controllers/usersController';
+import {
+  authCompleteProfile,
+  getUserData,
+} from '../controllers/usersController';
 
 const usersRouter = Router();
 
+usersRouter.get('/me', authenticate, getUserData);
 usersRouter.patch('/me', authenticate, authCompleteProfile);
 
 export default usersRouter;
