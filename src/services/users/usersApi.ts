94cbb86f89cead3e8593aca653b4d59a3e725195
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-03-12 23:50:20 +0300

    feat(profile-api): add api function for getting user data (#140)

--- src/services/users/usersApi.ts
+++ src/services/users/usersApi.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import type {
   CompleteProfileRequest,
   CompleteProfileResponse,
+  GetCurrentUserResponse,
 } from '@/types/users';
 
 const API_URL = import.meta.env.VITE_API_URL;
@@ -30,5 +31,30 @@ export const completeProfile = async (
     }
 
     throw new Error('Ошибка заполнения данных');
+  }
+};
+
+export const getCurrentUser = async (
+  token: string
+): Promise<GetCurrentUserResponse> => {
+  try {
+    const response = await axios.get<GetCurrentUserResponse>(
+      `${API_URL}/users/me`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Ошибка получения данных пользователя', error);
+
+    if (axios.isAxiosError(error)) {
+      const message = error.response?.data?.message;
+      throw new Error(message || 'Ошибка получения данных пользователя');
+    }
+
+    throw new Error('Ошибка получения данных пользователя');
   }
 };
