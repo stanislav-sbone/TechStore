8bd68b4e51c157b02008ecb8d2a92e0227c4e941
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-03-16 22:52:46 +0300

    refactor(navigation): add hasBadge flag for dynamic generate (#166)

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,25 +1,45 @@
 import { ROUTES } from '@/routes/constants/routes';
-import { Heart, Home, ShoppingCart, User } from 'lucide-react';
+import {
+  Heart,
+  Home,
+  ShoppingCart,
+  User,
+  type LucideProps,
+} from 'lucide-react';
+import type { ForwardRefExoticComponent, RefAttributes } from 'react';
 
-export const navItems = [
+interface NavItem {
+  path: string;
+  icon: ForwardRefExoticComponent<
+    Omit<LucideProps, 'ref'> & RefAttributes<SVGSVGElement>
+  >;
+  label: string;
+  hasBadge: boolean;
+}
+
+export const navItems: NavItem[] = [
   {
     path: ROUTES.HOME,
     icon: Home,
     label: 'Главная',
+    hasBadge: false,
   },
   {
     path: ROUTES.FAVORITES,
     icon: Heart,
     label: 'Избранное',
+    hasBadge: true,
   },
   {
     path: ROUTES.CART,
     icon: ShoppingCart,
     label: 'Корзина',
+    hasBadge: true,
   },
   {
     path: ROUTES.PROFILE,
     icon: User,
     label: 'Профиль',
+    hasBadge: false,
   },
 ] as const;
