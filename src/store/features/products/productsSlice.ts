e0cccbf3bd5215166f9f5eeeb7e3fdbe38521938
Станислав Борисенко <stas.borisenko91@gmail.com>
2026-03-16 16:00:43 +0300

    refactor(products): handle products fetching via async thunk in slice

--- src/store/features/products/productsSlice.ts
+++ src/store/features/products/productsSlice.ts
@@ -1,25 +1,56 @@
-import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import {
+  createAsyncThunk,
+  createSlice,
+  type PayloadAction,
+} from '@reduxjs/toolkit';
 import type { Product, ProductCategory } from '@/types/product';
+import { fetchProducts } from '@/services/products/productsApi';
+import axios from 'axios';
 
 interface ProductsState {
   items: Product[];
   searchQuery: string;
   category: ProductCategory | 'all';
+  error: string | null;
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed';
 }
+
+export const getProducts = createAsyncThunk<
+  Product[],
+  void,
+  { rejectValue: string }
+>('products/getProducts', async (_, { rejectWithValue }) => {
+  try {
+    const data = await fetchProducts();
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(error.message);
+      return rejectWithValue('Ошибка загрузки данных');
+    }
+
+    if (error instanceof Error) {
+      console.error(error.message);
+      return rejectWithValue('Ошибка загрузки данных');
+    }
+
+    return rejectWithValue('Ошибка загрузки данных');
+  }
+});
 
 const initialState: ProductsState = {
   items: [],
   searchQuery: '',
   category: 'all',
+  error: null,
+  loading: 'idle',
 };
 
 const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {
-    setProducts: (state, action: PayloadAction<Product[]>) => {
-      state.items = action.payload;
-    },
     setSearchQuery: (state, action: PayloadAction<string>) => {
       state.searchQuery = action.payload;
     },
@@ -27,8 +58,24 @@ const productsSlice = createSlice({
       state.category = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.pending, (state) => {
+        state.loading = 'pending';
+        state.error = null;
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.loading = 'succeeded';
+        state.error = null;
+        state.items = action.payload;
+      })
+      .addCase(getProducts.rejected, (state, action) => {
+        state.loading = 'failed';
+        state.error =
+          action.payload ?? action.error.message ?? 'Ошибка загрузки данных';
+      });
+  },
 });
 
-export const { setProducts, setSearchQuery, setCategory } =
-  productsSlice.actions;
+export const { setSearchQuery, setCategory } = productsSlice.actions;
 export default productsSlice.reducer;
