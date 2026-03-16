4fcf86d655f4aa74222d3a546de9748f7b212102
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-03-16 16:54:37 +0300

    feat(header-nav): add indicator to active page (#165)

--- src/components/Header/components/HeaderNav/HeaderNav.tsx
+++ src/components/Header/components/HeaderNav/HeaderNav.tsx
@@ -1,36 +1,37 @@
-import { Heart, ShoppingCart, User } from 'lucide-react';
-import { Link } from 'react-router';
+import { Link, useLocation } from 'react-router';
 import { ROUTES } from '@/routes/constants/routes';
 import { useAppSelector } from '@/store/hooks';
+import { navItems } from '@/constants/navigation';
 import styles from './HeaderNav.module.css';
 
 const HeaderNav = () => {
+  const { pathname } = useLocation();
   const favorites = useAppSelector((state) => state.favorites.items);
   const cart = useAppSelector((state) => state.cart.items);
 
   const cartQuantity = cart.reduce((acc, cur) => acc + cur.quantity, 0);
 
+  const getBadgeQuantity = (path: string) => {
+    if (path === ROUTES.FAVORITES) return favorites.length;
+    if (path === ROUTES.CART) return cartQuantity;
+  };
+
   return (
     <nav className={styles.nav}>
-      <Link to={ROUTES.FAVORITES} className={styles.buttonLink}>
-        <Heart color="#4a5568" size={27} />
-        {favorites.length > 0 && (
-          <span className={styles.badge}>
-            {favorites.length > 99 ? '99' : favorites.length}
-          </span>
-        )}
-      </Link>
-      <Link to={ROUTES.CART} className={styles.buttonLink}>
-        <ShoppingCart color="#4a5568" size={27} />
-        {cartQuantity > 0 && (
-          <span className={styles.badge}>
-            {cartQuantity > 99 ? '99' : cartQuantity}
-          </span>
-        )}
-      </Link>
-      <Link to={ROUTES.PROFILE} className={styles.buttonLink}>
-        <User color="#4a5568" size={27} />
-      </Link>
+      {navItems.map((item) => {
+        if (item.label === 'Главная') return;
+        const isActive = item.path === pathname;
+        const badgeCount = getBadgeQuantity(item.path) || 0;
+
+        return (
+          <Link to={item.path} key={item.path} className={styles.buttonLink}>
+            <item.icon color={isActive ? '#3182ce' : '#4a5568'} size={27} />
+            {badgeCount !== 0 && (
+              <span className={styles.badge}>{badgeCount}</span>
+            )}
+          </Link>
+        );
+      })}
     </nav>
   );
 };
