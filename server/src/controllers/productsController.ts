9a88b42b0fa61fb41712db73d26205e423b1613a
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-03-26 15:13:02 +0300

    feat(server-products): connect drizzle and postgres to products services (#178)

--- server/src/controllers/productsController.ts
+++ server/src/controllers/productsController.ts
@@ -1,27 +1,47 @@
 import { Request, Response } from 'express';
-import { products } from '../data/products';
-import { findProductById } from '../services/productsService';
+import { getAllProducts, getProduct } from '../services/productsService';
 
-export const getProducts = (_req: Request, res: Response): void => {
-  res.json(products);
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await getAllProducts();
+
+    return res.status(200).json(products);
+  } catch (error) {
+    console.error('Get products data error', error);
+
+    return res.status(500).json({
+      message:
+        error instanceof Error ? error.message : 'Ошибка получения товаров',
+    });
+  }
 };
 
-export const getProductById = (req: Request, res: Response): void => {
-  const { productId } = req.params;
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const { productId } = req.params;
 
-  const id = Number(productId);
+    const id = Number(productId);
 
-  if (Number.isNaN(id)) {
-    res.status(400).json({ message: 'Некорректный идентификатор товара' });
-    return;
+    if (Number.isNaN(id)) {
+      res.status(400).json({ message: 'Некорректный идентификатор товара' });
+      return;
+    }
+
+    const product = await getProduct(id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: 'Товар не найден',
+      });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.error('Get products data error', error);
+
+    return res.status(500).json({
+      message:
+        error instanceof Error ? error.message : 'Ошибка получения товара',
+    });
   }
-
-  const product = findProductById(id);
-
-  if (!product) {
-    res.status(404).json({ message: 'Товар не найден' });
-    return;
-  }
-
-  res.json(product);
 };
--- server/src/services/productsService.ts
+++ server/src/services/productsService.ts
@@ -1,12 +1,44 @@
-import { products } from '../data/products';
-import type { Product } from '../types/product';
+import { eq } from 'drizzle-orm';
+import { db } from '../db/connection';
+import { products } from '../db/schema/products';
 
-export const findProductById = (productId: number): Product | null => {
-  const product = products.find((productItem) => productItem.id === productId);
+export const getAllProducts = async () => {
+  return await db
+    .select({
+      id: products.id,
+      title: products.title,
+      brand: products.brand,
+      description: products.description,
+      price: products.price,
+      category: products.category,
+      images: products.images,
+      rating: products.rating,
+      inStock: products.in_stock,
+      isNew: products.is_new,
+      discount: products.discount,
+      specs: products.specs,
+    })
+    .from(products);
+};
 
-  if (!product) {
-    return null;
-  }
+export const getProduct = async (id: number) => {
+  const product = await db
+    .select({
+      id: products.id,
+      title: products.title,
+      brand: products.brand,
+      description: products.description,
+      price: products.price,
+      category: products.category,
+      images: products.images,
+      rating: products.rating,
+      inStock: products.in_stock,
+      isNew: products.is_new,
+      discount: products.discount,
+      specs: products.specs,
+    })
+    .from(products)
+    .where(eq(products.id, id));
 
-  return product;
+  return product[0] ?? null;
 };
