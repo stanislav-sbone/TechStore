4ec4aacf68a213fb96d170063db01ab22a85b04d
Станислав Борисенко <stas.borisenko91@gmail.com>
2026-04-05 23:54:15 +0300

    feat(server): add controller and service function for creating user order

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,9 +1,11 @@
-import { eq } from 'drizzle-orm';
+import { eq, inArray } from 'drizzle-orm';
 import { db } from '../db/connection';
 import { users } from '../db/schema/users';
 import { CartItem } from '../types/cart';
 import { usersFavorites } from '../db/schema/favorites';
 import { usersCart } from '../db/schema/cart';
+import { orders } from '../db/schema/orders';
+import { products } from '../db/schema/products';
 
 interface updateCurrentUserParams {
   userId: string;
@@ -139,4 +141,88 @@ export const setCartByUserId = async (userId: string, items: CartItem[]) => {
   return {
     items: updatedCart[0].items ?? [],
   };
+};
+
+export const createUserOrder = async (userId: string) => {
+  const newOrder = await db.transaction(async (tx) => {
+    const cart = await tx
+      .select()
+      .from(usersCart)
+      .where(eq(usersCart.user_id, userId));
+
+    if (cart.length === 0) {
+      throw new Error('Корзина не найдена');
+    }
+
+    const items = cart[0]?.items ?? [];
+
+    if (items.length === 0) {
+      throw new Error('Корзина пуста');
+    }
+
+    const productIds = items.map((item) => item.productId);
+
+    const productsData = await tx
+      .select({
+        id: products.id,
+        title: products.title,
+        images: products.images,
+        price: products.price,
+        discount: products.discount ?? 0,
+      })
+      .from(products)
+      .where(inArray(products.id, productIds));
+
+    const productsMap = new Map(
+      productsData.map((product) => [product.id, product])
+    );
+
+    const cartItemsWithData = items.map((item) => {
+      const product = productsMap.get(item.productId);
+
+      if (!product) {
+        throw new Error(`Товар ${item.productId} не найден`);
+      }
+
+      return {
+        productId: product.id,
+        title: product.title,
+        image: product.images?.[0] ?? '',
+        quantity: item.quantity,
+        price:
+          product.discount !== null
+            ? Math.round(product.price * (1 - product.discount))
+            : product.price,
+      };
+    });
+
+    const totalAmount = cartItemsWithData.reduce(
+      (sum, item) => sum + item.price * item.quantity,
+      0
+    );
+
+    const [order] = await tx
+      .insert(orders)
+      .values({
+        user_id: userId,
+        items: cartItemsWithData,
+        totalAmount: totalAmount,
+      })
+      .returning({
+        orderId: orders.orderNumber,
+        userId: orders.user_id,
+        items: orders.items,
+        totalAmount: orders.totalAmount,
+        created_at: orders.created_at,
+      });
+
+    await tx
+      .update(usersCart)
+      .set({ items: [] })
+      .where(eq(usersCart.user_id, userId));
+
+    return order;
+  });
+
+  return newOrder;
 };
