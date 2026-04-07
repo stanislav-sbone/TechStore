14c9559676c43f49e2627cb93065b2d8b267c7e7
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-04-07 15:12:40 +0300

    fix(edit-profile): add limit for firstName, lastName and email in zod schema (#206)

--- src/pages/Profile/components/ProfileEditModal/editProfile.schema.ts
+++ src/pages/Profile/components/ProfileEditModal/editProfile.schema.ts
@@ -1,15 +1,27 @@
 import z from 'zod';
 
 export const editProfileSchema = z.object({
-  firstName: z.string().trim().min(2, 'Заполните имя'),
-  lastName: z.string().trim().min(2, 'Заполните фамилию'),
+  firstName: z
+    .string()
+    .trim()
+    .min(2, 'Заполните имя')
+    .max(30, 'Слишком длинное имя'),
+  lastName: z
+    .string()
+    .trim()
+    .min(2, 'Заполните фамилию')
+    .max(30, 'Слишком длинная фамилия'),
   phone: z
     .string()
     .trim()
     .min(1, 'Заполните номер телефона')
     .regex(/^(?:\+7|8|7)\d{10}$/, 'Введите корректный номер телефона'),
   address: z.string().trim().min(5, 'Заполните адрес доставки'),
-  email: z.email('Введите корректный email').trim().min(1, 'Заполните email'),
+  email: z
+    .email('Введите корректный email')
+    .trim()
+    .min(1, 'Заполните email')
+    .max(255, 'Слишком длинный email'),
 });
 
 export type EditProfileData = z.infer<typeof editProfileSchema>;
