77b1993bd4ce1e9c1b1401183ce23aec1a180660
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-04-08 21:43:22 +0300

    feat(orders): add orders pagination (#210)

--- src/pages/Profile/components/Orders/Orders.tsx
+++ src/pages/Profile/components/Orders/Orders.tsx
@@ -1,7 +1,9 @@
-import { type FC } from 'react';
+import { useState, type FC } from 'react';
 import type { UserOrders } from '@/types/order';
 import OrderCard from '../OrderCard/OrderCard';
 import styles from './Orders.module.css';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import useIsMobile from '@/hooks/useIsMobile';
 
 interface OrdersProps {
   orders: UserOrders[];
@@ -10,6 +12,18 @@ interface OrdersProps {
 }
 
 const Orders: FC<OrdersProps> = ({ orders, loading, error }) => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const ordersPerPage = 5;
+  const lastOrderIndex = currentPage * ordersPerPage;
+  const firstOrderIndex = lastOrderIndex - ordersPerPage;
+  const totalPages = Math.ceil(orders.length / ordersPerPage);
+
+  const currentOrders = orders.slice(firstOrderIndex, lastOrderIndex);
+  const pages = new Array(totalPages).fill(null);
+
+  const isMobile = useIsMobile(450);
+
   if (loading) {
     return (
       <div className={styles.orders}>
@@ -41,7 +55,7 @@ const Orders: FC<OrdersProps> = ({ orders, loading, error }) => {
     <div className={styles.orders}>
       <h3 className={styles.title}>История заказов</h3>
       <div className={styles.ordersList}>
-        {orders.map((order) => (
+        {currentOrders.map((order) => (
           <OrderCard
             key={order.id}
             items={order.items}
@@ -51,6 +65,39 @@ const Orders: FC<OrdersProps> = ({ orders, loading, error }) => {
           />
         ))}
       </div>
+      {totalPages > 1 && (
+        <div className={styles.pagination}>
+          <button
+            className={styles.chevron}
+            disabled={currentPage === 1}
+            onClick={() => setCurrentPage((prev) => prev - 1)}
+          >
+            <ChevronLeft size={isMobile ? 20 : 24} />
+          </button>
+          <div className={styles.pages}>
+            {pages.map((_, i) => (
+              <button
+                key={i}
+                className={
+                  currentPage === i + 1
+                    ? `${styles.pageButton} ${styles.currentPage}`
+                    : styles.pageButton
+                }
+                onClick={() => setCurrentPage(i + 1)}
+              >
+                {i + 1}
+              </button>
+            ))}
+          </div>
+          <button
+            className={styles.chevron}
+            disabled={currentPage === totalPages}
+            onClick={() => setCurrentPage((prev) => prev + 1)}
+          >
+            <ChevronRight size={isMobile ? 20 : 24} />
+          </button>
+        </div>
+      )}
     </div>
   );
 };
