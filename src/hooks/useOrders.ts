f66fa493257f25d8ebfb8f36d73fa8f299dc1acb
Stanislav <127185518+stanislav-sbone@users.noreply.github.com>
2026-04-09 14:39:52 +0300

    refactor(orders): move pagination logic into useOrder hook (#212)

--- src/hooks/useOrders.ts
+++ src/hooks/useOrders.ts
@@ -5,9 +5,30 @@ import { useEffect, useState } from 'react';
 
 export const useOrders = () => {
   const [orders, setOrders] = useState<UserOrders[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
   const token = useAppSelector((state) => state.auth.token);
+
+  const ordersPerPage = 5;
+  const lastOrderIndex = currentPage * ordersPerPage;
+  const firstOrderIndex = lastOrderIndex - ordersPerPage;
+  const totalPages = Math.ceil(orders.length / ordersPerPage);
+
+  const currentOrders = orders.slice(firstOrderIndex, lastOrderIndex);
+  const pages = new Array(totalPages).fill(null);
+
+  const incrementPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const decrementPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -32,5 +53,16 @@ export const useOrders = () => {
     fetchOrders();
   }, [token]);
 
-  return { orders, loading, error };
+  return {
+    orders,
+    loading,
+    error,
+    currentPage,
+    currentOrders,
+    totalPages,
+    pages,
+    setCurrentPage,
+    incrementPage,
+    decrementPage,
+  };
 };
